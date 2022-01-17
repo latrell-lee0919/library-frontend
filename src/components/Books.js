e7e17972eecb4e652bb874a94e3f7cd96c1f7658
Latrell <lll4xy@virginia.edu>
2022-01-17 11:58:00 -0500

    8.19

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+  const [filter, setFilter] = useState('')
+
   if (!props.show) {
     return null
   }
 
   const books = props.books
+  console.log(books)
+
+  const genres = books.map(b => b.genres)
+  const uniqueGenres = [...new Set(genres.flat())]
+  const filteredBooks = books.filter(b => b.genres.includes(filter))
+
+  if (!filter) {
+    return (
+      <div>
+        <h2>books</h2>
+  
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                author
+              </th>
+              <th>
+                published
+              </th>
+            </tr>
+            {books.map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        {uniqueGenres.map(g =>
+          <button onClick={({ target }) => setFilter(target.value)} value={g} key={g}>
+            {g}
+          </button>)}
+      </div>
+    )
+  }
 
   return (
     <div>
       <h2>books</h2>
+     
+      <p>in genre <b>{filter}</b></p>
 
       <table>
         <tbody>
@@ -22,7 +64,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +73,10 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {uniqueGenres.map(g =>
+        <button onClick={({ target }) => setFilter(target.value)} value={g} key={g}>
+          {g}
+        </button>)}
     </div>
   )
 }
